d2f4b00e59dd023309cb132cedb27e0a0cd9697b
David Swatzell <thatsstrikethree@gmail.com>
2023-12-08 16:45:25 -0600

    test

--- src/main.js
+++ src/main.js
@@ -166,4 +166,6 @@ function showMain(){
 function viewSaved(){
   document.querySelector('.saved-posters').classList.toggle("hidden")
   document.querySelector(".main-poster").classList.toggle("hidden")
-}
+  }
+
+  hello there
